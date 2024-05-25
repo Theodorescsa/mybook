ac0804b86ae44a5a3cdfcfd41484405d565c11cf
Theodorescsa <128050459+Theodorescsa@users.noreply.github.com>
2024-05-25 22:24:32 +0700

    Update urls.py

--- mybook/partner/urls.py
+++ mybook/partner/urls.py
@@ -5,5 +5,5 @@ app_name = 'partner'
 urlpatterns = [
     path('',views.partner_list,name = "list"),
     path('api/',views.list_api,name = 'list_api'),
-    path('<int:id?/',views.partner_detail,name = "detail"),  
+    path('<int:id>/',views.partner_detail,name = "detail"),  
 ]
